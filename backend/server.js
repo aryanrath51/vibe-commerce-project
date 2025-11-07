cece2237c9d59db291ad1a5dc9de16cdc299085c
aryanrath51 <aryanrath51@gmail.com>
2025-11-07 22:34:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
-
 const app = express();
 const PORT = process.env.PORT || 5001;
-
 app.use(cors());
 app.use(express.json());
-
 app.set('cart', []);
 app.use('/api/products', productRoutes);
 app.use('/api', cartRoutes);
-
 app.get('/', (req, res) => {
   res.send('Welcome to the Vibe Commerce Mock API!');
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
